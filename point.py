0ba47cb31bf3ddb8e0c1b0f75957a510e775a1db
daveripper0020 <daveripper0020@gmail.com>
2024-01-23 01:40:23 +0900

    Point 해싱 구현

--- point.py
+++ point.py
@@ -6,6 +6,9 @@ class Point():
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
 
+    def __hash__(self):
+        return hash(f"{self.x},{self.y}")
+
     def __repr__(self):
         return f"({self.x}, {self.y})"
 
