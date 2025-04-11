d11cb81d6aedc03ca12603c226b7cd38b4c72cd8
daveripper0020 <daveripper0020@gmail.com>
2025-04-11 16:07:21 +0900

    카드 비교 개선

--- card.py
+++ card.py
@@ -3,18 +3,23 @@ from copy import deepcopy
 from enums import Faction
 from typing import TYPE_CHECKING
 from point import Point
+import uuid
 
 if TYPE_CHECKING:
     from player import Player
 
 class Card(ABC):
     def __init__(self):
+        self.uuid = uuid.uuid4()
         self.card_id = self.__class__.__name__.lower()[:4]
         self.player: Player = None
         self.faction: Faction = Faction.NEUTRAL
         self.cost = 0
         self.weight = 0
         self.is_single_use = False
+
+    def __eq__(self, other):
+        return self.uuid == other.uuid if isinstance(other, Card) else False
 
     def __int__(self):
         if self.card_id[:2] == "cs":
